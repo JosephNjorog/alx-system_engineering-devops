50dc60f429b9485a693300515f576d62dc7ddf1a
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2024-06-20 01:15:14 +0300

    All done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,9 @@
-# This configuration increases the amount of traffic an NGINX server can handle
+# Fix nginx to many failed requests
+exec {'increase_workers':
+command => '/bin/sed -i "s/worker_processes 4/worker_processes 8/g" /etc/nginx/nginx.conf',
+notify  => Service['nginx'],
+}
 
-# Increase the LIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/',
+service {'nginx':
+ensure => 'running',
 }
